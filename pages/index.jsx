6d1e5e49151f648eea40bca1fc2586766e2a405c
machadomatt <matheusmacsyn@gmail.com>
2022-01-25 14:29:16 -0300

    Add reactive content

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import clouds from '../public/clouds-bg.jpg'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import aurora from '../public/aurora-borealis.jpg'
 
 export default function Home() {
-    const username = 'machadomatt'
+    const [username, setUsername] = useState(['machadomatt'])
+    const route = useRouter()
 
     return (
         <div
@@ -21,18 +23,28 @@ export default function Home() {
                         Boas-vindas!
                     </h1>
                     <h2 className="mb-5">Aluracord - Matt in the sky</h2>
-                    <div className="flex flex-col items-center">
+                    <form
+                        className="flex flex-col items-center"
+                        onSubmit={(event) => {
+                            event.preventDefault()
+                            route.push('/chat')
+                        }}
+                    >
                         <input
                             className="self-stretch px-3 py-1 mb-6 transition duration-200 rounded-full shadow-md bg-white/20 backdrop-blur-lg focus:outline-none hover:bg-white/60 focus:shadow-lg"
                             type="text"
+                            value={username}
+                            onChange={(event) =>
+                                setUsername(event.target.value)
+                            }
                         />
                         <button
                             className="py-1 transition duration-200 rounded-full shadow-md px-7 bg-white/25 backdrop-blur-md hover:bg-white/60 hover:shadow-lg active:shadow-md"
-                            type="button"
+                            type="submit"
                         >
                             Entrar
                         </button>
-                    </div>
+                    </form>
                 </div>
                 <div className="flex flex-col items-center max-w-xs">
                     <Image
