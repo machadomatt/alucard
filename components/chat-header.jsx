a244c5aca733c21b1633987a183b00447577645f
machadomatt <matheusmacsyn@gmail.com>
2022-01-28 00:56:57 -0300

    Update Chat Header styles

--- components/chat-header.jsx
+++ components/chat-header.jsx
@@ -3,8 +3,17 @@ import Link from 'next/link'
 export default function ChatHeader() {
     return (
         <div className="flex justify-between mb-5">
-            <h1>Chat</h1>
-            <Link href="/">Logout</Link>
+            <h1 className="px-4 py-2 rounded-lg pointer-events-none bg-white/5 backdrop-blur-lg">
+                Chat
+            </h1>
+            <Link href="/">
+                <a
+                    className="px-4 py-2 transition duration-200 rounded-lg bg-white/5 backdrop-blur-lg hover:bg-white/60"
+                    href=""
+                >
+                    Logout
+                </a>
+            </Link>
         </div>
     )
 }
